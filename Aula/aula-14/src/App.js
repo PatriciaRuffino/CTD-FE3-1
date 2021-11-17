7ffedb8939f1174e0b11d27bbd5194a40d841caa
PatriciaRuffino <sindelformr@gmail.com>
2021-11-17 19:32:26 -0300

    "exercicio de mesa"

--- Aula/aula-14/src/App.js
+++ Aula/aula-14/src/App.js
@@ -8,28 +8,19 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      repositorios: []
+      cepNumber: []
     }
   }
 
-  async componentDidMount() {
-    try {
-      const response = await api.get(`/users/lmaoclost/repos`);
-      this.setState({ repositorios: response.data });
-    } catch (error) {
-      Swal.fire({
-        title: error.response.status,
-        icon: 'error',
-        text: error.response.data.message
-      });
-    }
-  }
+  
 
-  handleSubmit = async ({ nomeUsuario }) => {
-    if (nomeUsuario !== '') {
+  handleSubmit = async ({ cepNumber }) => {
+    console.log()
+    if (cepNumber !== '') {
       try {
-        const response = await api.get(`/users/${nomeUsuario}/repos`);
-        this.setState({ repositorios: response.data });
+        const response = await api.get(`/${cepNumber}/json`);
+        this.setState({ cepNumber: response.data });
+        console.log(response.data)
       } catch (error) {
         Swal.fire({
           title: error.response.status,
@@ -45,29 +36,30 @@ export default class App extends Component {
       <>
         <main>
           <div className="col-md-4 col-sm-6 my-3 container text-center">
-            <h2>Procure um usuário do Github</h2>
-            <Formik initialValues={{ nomeUsuario: '' }} onSubmit={this.handleSubmit}>
+            <h2>Procure um CEP</h2>
+            <Formik initialValues={{ cepNumber: '' }} onSubmit={this.handleSubmit}>
               <Form>
-                <Field placeholder="Insira o nome do usuário" required type="text" name="nomeUsuario" id="nomeUsuario" className="form-control" />
+                <Field placeholder="Insira o cep" required type="text" name="cepNumber" id="cepNumber" className="form-control" />
                 <button type="submit" className="btn btn-primary my-4">Pesquisar</button>
               </Form>
             </Formik>
-            {this.state.repositorios && (
-              <ul>
-                {this.state.repositorios.map(({ id, name, html_url }) => {
-                  return (
-                    <li key={id}>
-                      <a target="_blank" href={html_url} rel="noreferrer">
-                        {name}
-                      </a>
-                    </li>
-                  )
-                })}
-              </ul>
-            )}
           </div>
+         <ul>
+           <li>bairro: {this.state.cepNumber.bairro}</li>
+           <li>cep: {this.state.cepNumber.cep}</li>
+           <li>ddd: {this.state.cepNumber.ddd}</li>
+           <li>logradouro: {this.state.cepNumber.logradouro}</li>
+           <li>uf: {this.state.cepNumber.uf}</li>
+           <li>localidade: {this.state.cepNumber.localidade}</li>
+           </ul>
+           
+
         </main>
+          
       </>
     );
   }
 }
+         
+           
+           
